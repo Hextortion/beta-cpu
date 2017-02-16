83999b74b9894206d5a6c2d96d1a81fec666083c
Stefan Dumitrescu <stefandumitrescu@hotmail.com>
2017-02-16 12:41:12 -0500

    Reformat comments in reg_file.v

--- rtl/reg_file.v
+++ rtl/reg_file.v
@@ -1,12 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+//  File name: reg_file.v
+//  Author: Stefan Dumitrescu
+//  
+//  Description: This file contains the register file implementation
+///////////////////////////////////////////////////////////////////////////////
+
 module reg_file(
-    input logic clk,            // Clock
-    input logic [5:0] ra1,      // Read address 1
-    input logic [5:0] ra2,      // Read address 2
-    output logic [31:0] rd1,    // Read data 1
-    output logic [31:0] rd2,    // Read data 2
-    input logic we,             // Write enable
-    input logic [5:0] wa,       // Write address
-    input logic [31:0] wd       // Write data
+    input logic clk,            // clock
+    input logic [5:0] ra1,      // read address 1
+    input logic [5:0] ra2,      // read address 2
+    output logic [31:0] rd1,    // read data 1
+    output logic [31:0] rd2,    // read data 2
+    input logic we,             // write enable
+    input logic [5:0] wa,       // write address
+    input logic [31:0] wd       // write data
 );
 
 logic [31:0] mem[0:31];
