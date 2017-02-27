68825dd8241fb498f0cc31452188d4ff886a2e70
Stefan Dumitrescu <stefandumitrescu@hotmail.com>
2017-02-26 22:22:47 -0500

    Begin a rewrite using plain old verilog
    This is so that I can use open source tools such as icarus verilog.
    I'm tired of using modelsim.

--- rtl/operand_mux.v
+++ rtl/operand_mux.v
@@ -6,59 +6,59 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 module operand_mux(
-    input logic [4:0] ra,                   // read address
-    input logic [31:0] rd_in,               // input into rd mux
-
-    input logic [31:0] ex_y_bypass,         // Y bypass from execute stage
-    input logic [31:0] ex_pc_bypass,        // PC bypass from execute stage
-
-    input logic [31:0] mem_y_bypass,        // Y bypass from memory access stage
-    input logic [31:0] mem_pc_bypass,       // PC bypass from memory access stage
-
-    input logic [31:0] wb_bypass,           // register write value from write back stage
-
-    input logic [4:0] rc_wb,                // Rc from write back stage
-    input logic [4:0] rc_mem,               // Rc from memory access stage
-    input logic [4:0] rc_ex,                // Rc from execute stage
-
-    input logic op_br_or_jmp_ex,            // instruction is BEQ, BNE, or JMP in execute stage
-    input logic op_br_or_jmp_mem,           // instruction is BEQ, BNE, or JMP in memory access stage
-
-    output logic [31:0] rd_out,             // output of the rd mux
-    output logic ra_eq_rc_wb,               // Ra = Rc_WB
-    output logic ra_eq_rc_mem,              // Ra = Rc_MEM
-    output logic ra_eq_rc_ex                // Ra = Rc_EX
+    input [4:0] ra,                 // read address
+    input [31:0] rd_in,             // input into rd mux
+    input [31:0] ex_y_bypass,       // Y bypass from execute stage
+    input [31:0] ex_pc_bypass,      // PC bypass from execute stage
+    input [31:0] mem_y_bypass,      // Y bypass from memory access stage
+    input [31:0] mem_pc_bypass,     // PC bypass from memory access stage
+    input [31:0] wb_bypass,         // reg write value from write back stage
+    input [4:0] rc_wb,              // Rc from write back stage
+    input [4:0] rc_mem,             // Rc from memory access stage
+    input [4:0] rc_ex,              // Rc from execute stage
+    input op_br_or_jmp_ex,          // BEQ, BNE, or JMP in execute stage
+    input op_br_or_jmp_mem,         // BEQ, BNE, or JMP in memory access stage
+    output reg [31:0] rd_out,       // output of the rd mux
+    output ra_eq_rc_wb,             // Ra = Rc_WB
+    output ra_eq_rc_mem,            // Ra = Rc_MEM
+    output ra_eq_rc_ex              // Ra = Rc_EX
 );
 
-logic ra_ex_bypass;
-logic ra_mem_bypass;
-logic ra_wb_bypass; 
-logic ra_reg_file;
-logic ra_zr;
+wire ra_eq_31;
+wire [31:0] ex_bypass;
+wire [31:0] mem_bypass;
 
-logic [4:0] operand_sel;
+//
+// Note that if the ST instruction is in the mem, wb, or ex stage, then the
+// corresponding rc (rc_wb, rc_mem, or rc_ex) will be set to 32. This should
+// result in rd_out = rd_in.
+//
+assign ra_eq_rc_wb = rc_wb == ra;
+assign ra_eq_rc_mem = rc_mem == ra;
+assign ra_eq_rc_ex = rc_ex == ra;
+assign ra_eq_31 = &ra;
 
-always_comb begin
-    ra_eq_rc_wb = rc_wb == ra;
-    ra_eq_rc_mem = rc_mem == ra;
-    ra_eq_rc_ex = rc_ex == ra;
-    ra_zr = 5'd31 == ra;
+assign ex_bypass = op_br_or_jmp_ex ? ex_pc_bypass : ex_y_bypass;
+assign mem_bypass = op_br_or_jmp_mem ? mem_pc_bypass : mem_y_bypass;
 
-    ra_ex_bypass = !ra_zr && ra_eq_rc_ex;
-    ra_mem_bypass = !ra_zr && !ra_eq_rc_ex && ra_eq_rc_mem;
-    ra_wb_bypass = !ra_zr && !ra_eq_rc_ex && !ra_eq_rc_mem && ra_eq_rc_wb;
-    ra_reg_file = !ra_zr && !ra_eq_rc_ex && !ra_eq_rc_mem && !ra_eq_rc_wb;
-
-    operand_sel = {ra_reg_file, ra_wb_bypass, ra_mem_bypass, 
-                   ra_ex_bypass, ra_zr};
-
-    case (operand_sel)
-        5'b10000: rd_out = rd_in;
-        5'b01000: rd_out = wb_bypass;
-        5'b00100: rd_out = op_br_or_jmp_mem ? mem_pc_bypass : mem_y_bypass;
-        5'b00010: rd_out = op_br_or_jmp_ex ? ex_pc_bypass : ex_y_bypass;
-        5'b00001: rd_out = 32'd0;
-        default: rd_out = 'x;
+always @(*) begin
+    case ({ra_eq_31, ra_eq_rc_ex, ra_eq_rc_mem, ra_eq_rc_wb})
+        4'b0000: rd_out = rd_in;
+        4'b0001: rd_out = wb_bypass;
+        4'b0010: rd_out = mem_bypass;
+        4'b0011: rd_out = mem_bypass;
+        4'b0100: rd_out = ex_bypass;
+        4'b0101: rd_out = ex_bypass;
+        4'b0110: rd_out = ex_bypass;
+        4'b0111: rd_out = ex_bypass;
+        4'b1000: rd_out = 32'd0;
+        4'b1001: rd_out = 32'd0;
+        4'b1010: rd_out = 32'd0;
+        4'b1011: rd_out = 32'd0;
+        4'b1100: rd_out = 32'd0;
+        4'b1101: rd_out = 32'd0;
+        4'b1110: rd_out = 32'd0;
+        4'b1111: rd_out = 32'd0;
     endcase
 end
 
